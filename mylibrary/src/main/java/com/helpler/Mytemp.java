0cfc54d8ed6214de3db06e76a5ef5f1b96c7d50d
Ankur-PC\Ankur <ankur.csoft@gmail.com>
2018-03-16 11:36:39 +0530

    Initial Commit

---
+++ mylibrary/src/main/java/com/helpler/Mytemp.java
@@ -0,0 +1,13 @@
+package com.helpler;
+
+import android.util.Log;
+
+/**
+ * Created by Ankur on 3/16/2018.
+ */
+
+public class Mytemp {
+    public void show_msg(){
+        Log.e("my_sg","msg");
+    }
+}
